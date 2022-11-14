9b001bc79412b708c8a2d7ebe10fda47de5ab224
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-11-14 17:31:57 +0000

    重大問題修正中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,23 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
   end
 
   def show
   end
+  
+  def create
+ 
+  end
 
   def edit
   end
+  
+    private
+  
+  def list_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+
+  
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ApplicationRecord
 
+
+
   belongs_to :user
+
+
 
 end
