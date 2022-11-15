216112de1831c1ad141070c64d0fe7407f66f936
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-11-15 15:27:15 +0000

    book showレイアウト完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,9 @@ class BooksController < ApplicationController
   end
 
   def show
-    
+    @books = Book.find(params[:id])
+    @user = User.find(@books.user_id)
+    @book = Book.new
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :books, only: [:index, :show, :edit, :create]
+  resources :books, only: [:index, :show, :edit, :create, :destroy]
   resources :users, only: [:show, :edit, :index]
 
   get 'homes/about' => 'homes#about', as: 'about'
