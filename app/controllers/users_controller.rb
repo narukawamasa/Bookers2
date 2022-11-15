eea92865638b46070ac1a95387f66c8adb8620f5
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-11-15 19:05:14 +0000

    ページレイアウトALLOK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   @book = Book.new
   @books = @user.books
   end
-  
+
   def index
     @book = Book.new
     @user = current_user
@@ -12,5 +12,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+    private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
