26a54a0093f6372abcfb64f3e7e771f768582020
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-11-20 08:51:54 +0000

    フォロー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root to: "homes#top"
 
@@ -8,7 +10,11 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :index, :update]
+  resources :users, only: [:show, :edit, :index, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   
 
   get 'home/about' => 'homes#about', as: 'about'
